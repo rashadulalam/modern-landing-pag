10ae0cdb2fa54399fce52a44f72ef4afbf0ac12a
rashadulalam <rashadul91@gmail.com>
2023-05-07 07:45:47 +0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
